06d3949c17c2679b4083217100532d086273f2cc
ayushpaine <ayushmt701@gmail.com>
2022-07-12 23:36:32 +0530

    top half of the header added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HeaderTop from "./HeaderTop";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header-wrapper">
+        <HeaderTop />
+      </div>
+    </>
+  );
+};
+
+export default Header;
