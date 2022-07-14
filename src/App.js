9f062adffe35ed85c34fa2d8bdf3ab75498fe283
ayushpaine <ayushmt701@gmail.com>
2022-07-15 01:27:11 +0530

    small fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import React from "react";
 import Header from "./components/Header/Header";
 import Title from "./components/Body/Title/Title";
 import Body from "./components/Body/Body";
